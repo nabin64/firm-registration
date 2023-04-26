e1b8a3db5146d46c2b42826d39884cf15a0a9191
nabin <lamanabin64@gmail.com>
2023-04-26 23:03:50 +0545

    registration changed

--- client/src/pages/home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const home = () => {
-    return (
-        <div>Hello from home.js</div>
-    )
-}
-
-export default home
--- client/src/pages/registration.js
+++ client/src/pages/registration.js
@@ -5,44 +5,17 @@ const RegistrationPage = () => {
 
     const [isAdmin, setIsAdmin] = useState(false);
 
-
-
     const handleToggle = () => {
         setIsAdmin(!isAdmin);
     };
 
-
-
     return (
         <div className={styles.container}>
-
-            <form  className={styles.Home_form}>
+            <form className={styles.Home_form}>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/1/1f/New_Emblem_of_Nepal_Red.png" alt="Government of Nepal" style={{ width: "100px", height: "80px" }} />
 
-                <h2>{isAdmin ? 'Admin Registration' : 'User Registration'}</h2>
-
-
-                <input type="text"  className={styles.Home_input} placeholder="Full Name" />
-
-
-
-                <input type="text"  className={styles.Home_input} placeholder="Address" />
-
-
-
-                <input type="text"  className={styles.Home_input} placeholder="Mobile No" />
-
-
-
-                <input type="email"  className={styles.Home_input} placeholder="Email address" style={{ width: "365px" }} />
-
-
-
-
-                <input type="password"  className={styles.Home_input} placeholder="Password" />
-
-                <button type="submit">Register Now</button>
                 <div className={styles.toggleContainer}>
+
                     <span>User</span>
                     <label className={styles.switch}>
                         <input type="checkbox" checked={isAdmin} onChange={handleToggle} />
@@ -50,6 +23,30 @@ const RegistrationPage = () => {
                     </label>
                     <span>Admin</span>
                 </div>
+
+                <h2>{isAdmin ? 'Admin Registration' : 'User Registration'}</h2>
+
+                {isAdmin ? (
+                    <>
+                        <input type="text" className={styles.Home_input} placeholder="Full Name" />
+                        <input type="text" className={styles.Home_input} placeholder="Address" />
+                        <input type="text" className={styles.Home_input} placeholder="Mobile No" />
+                        <input type="email" className={styles.Home_input} placeholder="Email address" style={{ width: "365px" }} />
+                        <input type="password" className={styles.Home_input} placeholder="Password" />
+
+                    </>
+                ) : (
+                    <>
+                        <input type="text" className={styles.Home_input} placeholder="Full Name" />
+                        <input type="text" className={styles.Home_input} placeholder="Address" />
+                        <input type="text" className={styles.Home_input} placeholder="Mobile No" />
+                        <input type="email" className={styles.Home_input} placeholder="Email address" style={{ width: "365px" }} />
+                        <input type="text" className={styles.Home_input} placeholder="PAN" />
+                        <input type="password" className={styles.Home_input} placeholder="Password" />
+                    </>
+                )}
+
+                <button type="submit">Register Now</button>
             </form>
         </div>
     );
