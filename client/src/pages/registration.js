5ff4b0163419bb8ccfff0cfaa767a5a829d8d767
nabin <lamanabin64@gmail.com>
2023-04-29 21:23:39 +0545

    formik and yup added

--- client/src/pages/registration.js
+++ client/src/pages/registration.js
@@ -1,6 +1,93 @@
+// import { useState } from 'react';
+// import styles from '../styles/Registration.module.css';
+// import { useRouter } from 'next/router';
+
+// class User {
+//     constructor({ fullName, address, mobileNo, email, password, userType }) {
+//         this.fullName = fullName;
+//         this.address = address;
+//         this.mobileNo = mobileNo;
+//         this.email = email;
+//         this.password = password;
+//         this.userType = userType;
+//     }
+// }
+
+// const RegistrationPage = () => {
+//     const [isAdmin, setIsAdmin] = useState(false);
+//     const router = useRouter();
+
+//     const handleToggle = () => {
+//         setIsAdmin(!isAdmin);
+//         console.log(isAdmin)
+//     };
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         const { fullName, address, mobileNo, email, password } = e.target;
+//         const newUser = new User({
+//             fullName: fullName.value,
+//             address: address.value,
+//             mobileNo: mobileNo.value,
+//             email: email.value,
+//             password: password.value,
+//             userType: isAdmin ? 'admin' : 'user',
+//         });
+//         try {
+//             const response = await fetch('http://localhost:4000/register', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify(newUser),
+//             });
+//             const data = await response.text();
+//             console.log(data);
+//             router.push('/login'); // Redirect to login page after successful registration
+//         } catch (err) {
+//             console.log(err);
+//         }
+//     };
+
+//     return (
+//         <div className={styles.container}>
+//             <form className={styles.Home_form} onSubmit={handleSubmit}>
+//                 <img
+//                     src="https://upload.wikimedia.org/wikipedia/commons/1/1f/New_Emblem_of_Nepal_Red.png"
+//                     alt="Government of Nepal"
+//                     style={{ width: '100px', height: '80px' }}
+//                 />
+
+//                 <div className={styles.toggleContainer}>
+//                     <span>User</span>
+//                     <label className={styles.switch}>
+//                         <input type="checkbox" checked={isAdmin} onChange={handleToggle} />
+//                         <span className={styles.slider}></span>
+//                     </label>
+//                     <span>Admin</span>
+//                 </div>
+
+//                 <h2>{isAdmin ? 'Admin Registration' : 'User Registration'}</h2>
+
+//                 <input type="text" className={styles.Home_input} name="fullName" placeholder="Full Name" />
+//                 <input type="text" className={styles.Home_input} name="address" placeholder="Address" />
+//                 <input type="text" className={styles.Home_input} name="mobileNo" placeholder="Mobile No" />
+//                 <input type="email" className={styles.Home_input} name="email" placeholder="Email address" style={{ width: '365px' }} />
+//                 <input type="password" className={styles.Home_input} name="password" placeholder="Password" />
+
+//                 <button type="submit">Register Now</button>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default RegistrationPage;
+
 import { useState } from 'react';
 import styles from '../styles/Registration.module.css';
 import { useRouter } from 'next/router';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 class User {
     constructor({ fullName, address, mobileNo, email, password, userType }) {
@@ -22,15 +109,29 @@ const RegistrationPage = () => {
         console.log(isAdmin)
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const { fullName, address, mobileNo, email, password } = e.target;
+    const initialValues = {
+        fullName: '',
+        address: '',
+        mobileNo: '',
+        email: '',
+        password: '',
+    };
+
+    const validationSchema = Yup.object({
+        fullName: Yup.string().required('Required'),
+        address: Yup.string().required('Required'),
+        mobileNo: Yup.string().required('Required').length(10, 'Mobile number must be exactly 10 digits long'),
+        email: Yup.string().email('Invalid email address').required('Required'),
+        password: Yup.string().required('Required'),
+    });
+
+    const handleSubmit = async (values) => {
         const newUser = new User({
-            fullName: fullName.value,
-            address: address.value,
-            mobileNo: mobileNo.value,
-            email: email.value,
-            password: password.value,
+            fullName: values.fullName,
+            address: values.address,
+            mobileNo: values.mobileNo,
+            email: values.email,
+            password: values.password,
             userType: isAdmin ? 'admin' : 'user',
         });
         try {
@@ -51,34 +152,50 @@ const RegistrationPage = () => {
 
     return (
         <div className={styles.container}>
-            <form className={styles.Home_form} onSubmit={handleSubmit}>
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/1/1f/New_Emblem_of_Nepal_Red.png"
-                    alt="Government of Nepal"
-                    style={{ width: '100px', height: '80px' }}
-                />
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+                {(formik) => (
+                    <Form className={styles.Home_form}>
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/1/1f/New_Emblem_of_Nepal_Red.png"
+                            alt="Government of Nepal"
+                            style={{ width: '100px', height: '80px' }}
+                        />
 
-                <div className={styles.toggleContainer}>
-                    <span>User</span>
-                    <label className={styles.switch}>
-                        <input type="checkbox" checked={isAdmin} onChange={handleToggle} />
-                        <span className={styles.slider}></span>
-                    </label>
-                    <span>Admin</span>
-                </div>
+                        <div className={styles.toggleContainer}>
+                            <span>User</span>
+                            <label className={styles.switch}>
+                                <Field type="checkbox" name="userType" checked={isAdmin} onChange={handleToggle} />
+                                <span className={styles.slider}></span>
+                            </label>
+                            <span>Admin</span>
+                        </div>
 
-                <h2>{isAdmin ? 'Admin Registration' : 'User Registration'}</h2>
+                        <h2>{isAdmin ? 'Admin Registration' : 'User Registration'}</h2>
 
-                <input type="text" className={styles.Home_input} name="fullName" placeholder="Full Name" />
-                <input type="text" className={styles.Home_input} name="address" placeholder="Address" />
-                <input type="text" className={styles.Home_input} name="mobileNo" placeholder="Mobile No" />
-                <input type="email" className={styles.Home_input} name="email" placeholder="Email address" style={{ width: '365px' }} />
-                <input type="password" className={styles.Home_input} name="password" placeholder="Password" />
+                        <Field type="text" className={styles.Home_input} name="fullName" placeholder="Full Name" />
+                        <ErrorMessage name="fullName" />
 
-                <button type="submit">Register Now</button>
-            </form>
+                        <Field type="text" className={styles.Home_input} name="address" placeholder="Address" />
+                        <ErrorMessage name="address" />
+
+                        <Field type="text" className={styles.Home_input} name="mobileNo" placeholder="Mobile No" />
+                        <ErrorMessage name="mobileNo" />
+
+                        <Field type="email" className={styles.Home_input} name="email" placeholder="Email address" style={{ width: '365px' }} />
+                        <ErrorMessage name="email" />
+
+                        <Field type="password" className={styles.Home_input} name="passworda" placeholder="Password"
+                        />
+                        <ErrorMessage name="password" />
+
+                        <button type="submit" disabled={!formik.isValid || formik.isSubmitting}>
+                            {formik.isSubmitting ? 'Submitting...' : 'Register Now'}
+                        </button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
-};
+}
 
-export default RegistrationPage;
+    export default RegistrationPage;
